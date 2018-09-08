e78efc3f0fa021a3882e7e8e236ba5e6fba0d55e
MSloan <msloan21@gmail.com>
2018-09-08 12:53:48 -0400

    responsive welcome and announcements

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -8,8 +8,8 @@ import Title from '../../components/Title/index';
 
 export default class HomePage extends React.PureComponent {
     render() {
-        let cardLeft = (window.innerWidth < 400) ? '' : 'card card-left'
-        let cardRight = (window.innerWidth < 400) ? '' : 'card card-right'
+        let cardLeft = (window.innerWidth < 400) ? 'card' : 'card card-left'
+        let cardRight = (window.innerWidth < 400) ? 'card' : 'card card-right'
 
         return (
             <div>
@@ -17,19 +17,19 @@ export default class HomePage extends React.PureComponent {
                 <Row className={'home'}>
                     <Home/>
                 </Row>
-                {/*<Row className={'wrapper'}>*/}
-                    {/*<div className={cardLeft}>*/}
-                        {/*<Title title={'Welcome'}/>*/}
-                        {/*<br/>*/}
-                        {/*<iframe width="100%" height="400" src="https://www.youtube.com/embed/LH1GFaw09hk"*/}
-                                {/*frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>*/}
-                    {/*</div>*/}
-                    {/*<div className={cardRight}>*/}
-                        {/*<Title title={'Announcements'}/>*/}
-                        {/*<br/>*/}
-                        {/*<Twitter/>*/}
-                    {/*</div>*/}
-                {/*</Row>*/}
+                <Row className={'wrapper'}>
+                    <div className={cardLeft}>
+                        <Title title={'Welcome'}/>
+                        <br/>
+                        <iframe width="100%" height="400" src="https://www.youtube.com/embed/LH1GFaw09hk"
+                                frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
+                    </div>
+                    <div className={cardRight}>
+                        <Title title={'Announcements'}/>
+                        <br/>
+                        <Twitter/>
+                    </div>
+                </Row>
                 {/*<div className={'ig-feed'}>*/}
                     {/*<Instagram />*/}
                 {/*</div>*/}
