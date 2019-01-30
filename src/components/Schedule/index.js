2ac4f978a42dda2cf0146e6668c822d76027e4c2
MSloan <msloan21@gmail.com>
2019-01-30 17:25:58 -0500

    initial schedule

---
+++ src/components/Schedule/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Row from "reactstrap/src/Row";
+import Col from "reactstrap/src/Col";
+
+export default class Schedule extends React.PureComponent {
+    render() {
+
+        return (
+            <Row>
+                Schedule
+            </Row>
+        );
+    }
+}
